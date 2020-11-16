72cd2a814464e06de50611b6277d061babac7469
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2020-11-15 23:03:44 -0300

    homepage ok

--- alchemist/src/App.vue
+++ alchemist/src/App.vue
@@ -1,53 +1,59 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app>  <!--DRAWER-->
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
+    <v-card>
+      <v-navigation-drawer v-model="drawer" app>  <!--DRAWER-->
+        <v-list-item>
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title @click="drawer=!drawer" class="title">
+              <v-icon>{{ 'fas fa-atom' }}</v-icon> Alchemist
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-      </v-list>
-    </v-navigation-drawer>    <!--FIM DRAWER-->
 
-    <v-app-bar app>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-divider></v-divider>
 
-        <v-toolbar-title>Alchemist</v-toolbar-title>
-    </v-app-bar>
+        <v-list dense nav>
+          <v-list-item v-for="link in links" :key="link.titulo" large>
+            <v-list-item-icon>
+              <v-icon>{{ link.icone }}</v-icon>
+            </v-list-item-icon>
 
-    <v-main>
-    </v-main>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{link.titulo}}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        
+      </v-navigation-drawer>    <!--FIM DRAWER-->
+    </v-card>
 
-  </v-app>
+      <v-app-bar app>     <!--NAV BAR-->
+          <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+
+          <v-toolbar-title>Alchemist</v-toolbar-title>
+      </v-app-bar>      <!--FIM NAV BAR-->
+
+      <v-main>
+          <router-view> </router-view>
+      </v-main>
+
+    </v-app>
 </template>
 
 <script>
   export default {
     data: () => ({ 
-      drawer:{},
-      items:[
-          { title: 'Home', icon: 'mdi-home-city' },
-          { title: 'My Account', icon: 'mdi-account' },
-          { title: 'Users', icon: 'mdi-account-group-outline' },
-        ],
+      drawer:false,
+      links:[
+        {rota:'/',titulo:'Home',icone:'fas fa-atom'},
+        {rota:'/cadastro',titulo:'Cadastro',icone:"fas fa-user-plus"},
+        {rota:'/login',titulo:'Login',icone:'fas fa-sign-in-alt'},
+        {rota:'/perfil',titulo:'Perfil',icone:'far fa-id-badge'},
+        {rota:'/sair',titulo:'Sair',icone:'fas fa-sign-out-alt'},
+      ],
+
     }),
   }
 </script>
---
+++ alchemist/src/components/Homepage.vue
@@ -0,0 +1,64 @@
+<template>
+    <v-container class="d-flex justify-center">
+        <div class="content">
+            <h5 class="card-title" style="font-size:50px;">Bem-vindo(a) à <strong>Alchemist</strong>!</h5>
+            <v-card class="mx-auto">
+                <v-card-actions v-if="isLogado==false">
+                    <router-link to="/login" replace>
+                        <v-btn outlined text class="ma-2 item" color="light">Entrar</v-btn>                    
+                    </router-link>
+                    <router-link to="/cadastro" replace> <v-btn outlined text class="item ma-2 item" color="secondary">Cadastrar</v-btn> </router-link>
+                </v-card-actions>
+                <v-card-actions v-else>
+                    <h5><strong>{{userAtual.user.name}}</strong> está logado!</h5>
+                    <router-link to="/perfil"><v-btn outlined text class="item ma-2 item" color="secondary">Perfil</v-btn>   </router-link>
+                </v-card-actions>
+            </v-card>
+        </div>
+    </v-container>
+</template>
+<script>
+export default {
+    name:'Homepage',
+    data() {
+        return {
+            nome:'',
+        }
+    },
+
+    computed: {
+      isLogado() {
+          if(this.nome!=''){ return true;  }
+          return false;
+      },
+      userAtual() { 
+            if(this.isLogado==true){
+                return JSON.parse(localStorage.getItem('user'));
+            }
+            return null;
+        }
+    }
+
+}
+</script>
+<style>
+   .item{
+        position: relative; font-size: 20px; text-decoration: none;
+        transition: 0.4s ease; margin: 0px 10px 0px 10px;
+    }
+    .item:hover{
+        color: #313238; font-style: italic; text-decoration: none; font-size: 22px;
+    }
+    .item:hover::after{
+        color: #313238; font-style: italic; text-decoration: none; font-size: 22px;
+    }
+    .links{
+        color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+    }
+</style>
